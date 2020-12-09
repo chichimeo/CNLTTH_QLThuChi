48b9e4cccd4b602b2bcdcaf0ca3e040f6c13b9bb
CamTu2611 <Camtunghiadan@gmail.com>
2020-12-10 00:27:47 +0700

    update _Layout, add images, Account

---
+++ QLThuChiCaNhan/DemoMVC/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+        public ActionResult RegisterAccount()
+        {
+
+            return View();
+        }
+        public ActionResult RegisterResult()
+        {
+
+            return RedirectToAction("Login", "Account");
+        }
+        public ActionResult Logout()
+        {
+
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
