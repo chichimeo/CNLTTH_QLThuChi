09a3f04de0f367597a691ebdd2f41eb5dcbb585f
CamTu2611 <Camtunghiadan@gmail.com>
2020-12-11 00:00:36 +0700

    update Account, Layout, HomeController and TuTuService

--- QLThuChiCaNhan/DemoMVC/Controllers/AccountController.cs
+++ QLThuChiCaNhan/DemoMVC/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoMVC.DangNhap;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,32 @@ using System.Web.Mvc;
 
 namespace DemoMVC.Controllers
 {
+    
     public class AccountController : Controller
     {
+        private TuTuService service = new TuTuService();
         // GET: Account
         public ActionResult Login()
         {
-
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = service.KiemTraDN(username, password);
+                if(user != null)
+                {
+                    Session["User"] = user;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.error = "Đăng nhập thất bại";
+                    return RedirectToAction("Login");
+                }
+            }
             return View();
         }
         public ActionResult Register()
@@ -19,19 +40,26 @@ namespace DemoMVC.Controllers
 
             return View();
         }
-        public ActionResult RegisterAccount()
+        public ActionResult RegisterResult()
         {
 
             return View();
         }
-        public ActionResult RegisterResult()
+        public ActionResult RegisterAccount(string name, string email, string username, string password)
         {
-
-            return RedirectToAction("Login", "Account");
+            if(service.ThemNguoiDung(name, email, username, password))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                ViewBag.error = "Đăng ký thất bại";
+                return View();
+            }
         }
         public ActionResult Logout()
         {
-
+            Session.Clear();
             return RedirectToAction("Login", "Account");
         }
     }
